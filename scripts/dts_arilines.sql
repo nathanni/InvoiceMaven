93c36a397c2b8a67d67bbc2d3a23dfba1bd0a8ce
Nathan Ni <nathanoct@gmail.com>
2016-02-15 17:47:51 -0500

    scripts added

---
+++ scripts/dts_arilines.sql
@@ -0,0 +1,16 @@
+ALTER TABLE mssql.dts_arilines
+ADD(WWLINE NUMBER(1));
+ALTER TABLE mssql.dts_arilines
+ADD(WIDTHES NUMBER(7,3));
+ALTER TABLE mssql.dts_arilines
+ADD(HEIGHTES NUMBER(7,3));
+ALTER TABLE mssql.dts_arilines
+ADD(L1_DESC VARCHAR2(500));
+ALTER TABLE mssql.dts_arilines
+ADD(L2_DESC VARCHAR2(500));
+ALTER TABLE mssql.dts_arilines
+ADD(L1_LINETYPEID NUMBER(5));
+ALTER TABLE mssql.dts_arilines
+ADD(L1_MUTYPE VARCHAR2(50));
+ALTER TABLE mssql.dts_arilines
+ADD(L3_MULLTYPE VARCHAR2(25));
