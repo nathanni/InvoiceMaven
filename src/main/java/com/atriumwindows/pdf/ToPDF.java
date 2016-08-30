eae2f2daec9df311eeaa4eff475f40c6545d0f5b
Nathan Ni <nathanoct@gmail.com>
2016-08-30 14:54:29 -0400

    bug fix: add footer to URI TO PDF

--- src/main/java/com/atriumwindows/pdf/ToPDF.java
+++ src/main/java/com/atriumwindows/pdf/ToPDF.java
@@ -128,7 +128,7 @@ public class ToPDF {
 
         String outputFile = tempPath + "temp" + System.currentTimeMillis() + ".pdf";
 
-        String params = "--footer-center %27Page: [page] / [topage]%27 --footer-line";
+        String params = "--footer-center [Page: [page] / [topage]] --footer-line";
 
         executeCmd(uri, outputFile, params, false);
 
